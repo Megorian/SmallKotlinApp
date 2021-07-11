0de5a08e950b41feb960deb4355e1d8283eea483
Igor Gusakov <auron058@gmail.com>
2021-07-11 23:09:41 +0300

    removed unused parts

--- app/src/main/java/ru/lentrodev/retrofitrxjavaexample/db/RoomDB.kt
+++ app/src/main/java/ru/lentrodev/retrofitrxjavaexample/db/RoomDB.kt
@@ -6,14 +6,13 @@ import ru.lentrodev.retrofitrxjavaexample.db.dao.CardDao
 import ru.lentrodev.retrofitrxjavaexample.db.dao.CardSetDao
 import ru.lentrodev.retrofitrxjavaexample.db.model.Card
 import ru.lentrodev.retrofitrxjavaexample.db.model.CardSet
-import ru.lentrodev.retrofitrxjavaexample.db.model.User
 
 /**
  * Created by Igor Gusakov on 06.07.2021.
  */
 @Database(
-    entities = [Card::class, CardSet::class, User::class],
-    version = 6
+    entities = [Card::class, CardSet::class],
+    version = 1
 )
  abstract class RoomDB: RoomDatabase() {
 
